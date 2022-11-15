1b3d6a0dfda8af6620c078c4310d845d08171bb5
Mateuswuz <116360372+Mateuswuz@users.noreply.github.com>
2022-11-15 10:46:22 -0300

    Add files via upload

---
+++ assets/preload/scripts/hide rika shit.lua
@@ -0,0 +1,6 @@
+function onCreatePost()
+	setProperty('scoreTxt.visible', false)
+    	setProperty('timeBarBG.visible', false)
+    	setProperty('timeBar.visible', false)
+   	setProperty('timeTxt.visible', false)
+end
